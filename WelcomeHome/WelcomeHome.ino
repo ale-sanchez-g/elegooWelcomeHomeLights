d2a06b8deab51157b1464e801c39f0a1bc98d8af
Alejandro Sanchez <alejandro.sanchez@farmbot.com.au>
2022-10-12 09:07:07 +1100

    change light show

--- WelcomeHome/WelcomeHome.ino
+++ WelcomeHome/WelcomeHome.ino
@@ -1,6 +1,6 @@
-const int bluePin = 13;
+const int bluePin = 7;
 const int redPin = 12;
-const int yellowPin = 9;
+const int yellowPin = 8;
 
 const int inputPin = 2;
 
@@ -16,34 +16,50 @@ void setup()
 
 void loop()
 {
-  int r = rand();
-  digitalWrite(bluePin, LOW);
-
   int value = digitalRead(inputPin);
   Serial.println("Motion Sensor value: " + String(value));
 
   if (value == HIGH)
   {
 
+    // Light show sequence
     const int SIZE = 12;
-    int prices[SIZE] = {1, 3, 3, 0, 1, 1, 1, 3, 3, 0, 1, 1};
-    int color[SIZE] = {'bluePin', 'redPin', 'redPin', 'yellowPin', 'bluePin', 'bluePin', 'bluePin', 'redPin', 'redPin', 'yellowPin', 'bluePin', 'bluePin'};
+    int prices[SIZE] = {1, 3, 3, 2, 1, 1, 1, 3, 3, 2, 1, 1};
+    int color[SIZE] = {bluePin, redPin, redPin, yellowPin, bluePin, bluePin, bluePin, redPin, redPin, yellowPin, bluePin, bluePin};
 
+    // Repease the light show 3 times
+    for (int j = 0; j < 3; j++)
+    {      
+      Serial.println("Round : " + String(j));
 
-    for (int i = 0; i < SIZE; i++)
+      // Light show
+      for (int i = 0; i < SIZE; i++)
+      {
+
+        digitalWrite(color[i], HIGH);
+        delay(prices[i] * 100);
+
+        digitalWrite(color[i], LOW);
+        Serial.println("delay time " + String(prices[i]));
+      }
+      
+    }
+    // Alert to the user about exit loop 
+    for (int j = 0; j < 3; j++)
     {
+      Serial.println("Exiting Now ------ bye");
 
-      digitalWrite(color[i], HIGH);
-      delay(prices[i] * 100);
-
-      digitalWrite(color[i], LOW);
+      digitalWrite(bluePin, HIGH);
+      digitalWrite(redPin, HIGH);
+      digitalWrite(yellowPin, HIGH);
       delay(1000);
-      Serial.println("delay time " + String(prices[i]));
+      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      delay(1000);
+
     }
   }
 
-  digitalWrite(bluePin, LOW);
-  digitalWrite(redPin, LOW);
-  digitalWrite(yellowPin, LOW);
-
+  delay(500);
 }
